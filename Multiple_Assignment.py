0bfa960e1d0e85c24b96bd1abc1aeb7c8115c9e1
RitickMadaan <ritickmadaan55@gmail.com>
2018-09-30 16:25:39 +0530

    multiple assignment

---
+++ Multiple_Assignment.py
@@ -0,0 +1,2 @@
+name,university_roll_number,email,contact="Ritick Madaan",1810991129,"ritickmadaan55",9056263665
+print("User Details :\n","Name :",name,"\n","University Roll Number :",university_roll_number,"\n","Email :",email,"\n","Contact :",contact,)
